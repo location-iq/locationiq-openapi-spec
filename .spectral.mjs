99a406b7871845c0dce77b7a26c44ce9509489a4
arvind@unwiredlabs.com <arvind@unwiredlabs.com>
2023-08-22 16:03:44 +0530

    Removed php for search

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNTE4Nw/spectral.js?branch_slug=master&token=c2c4b3cd-beba-4e13-be0f-37746330a11d";
+export default { extends: ruleset };
